b884c7af85e7f353429dff083228797f206d05e3
Kaja <catcalledkaja@gmail.com>
2019-08-27 11:51:24 +0100

    adding deposit and withdrawl

--- lib/account.rb
+++ lib/account.rb
@@ -7,4 +7,12 @@ attr_reader :balance
     @balance = 0
   end
 
+  def deposit(amount)
+    @balance += amount
+  end
+
+  def withdrawal(amount)
+    @balance -= amount
+  end
+
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -6,7 +6,21 @@ describe Account do
   context "New Account" do
     describe "#Initialize" do
       it "should have an empty balance" do
-        expect(account.balance).to eq 0
+        expect(account.balance).to eq(0)
+      end
+    end
+
+    describe "#Deposit amount to the account" do
+      it "should allow client to deposit amount" do
+        expect(account.deposit(100)).to eq(100)
+      end
+    end
+
+    describe "#Withdrawal amount" do
+      it "should allow client to withdrawal money from their account" do
+        account.deposit(100)
+        account.withdrawal(50)
+        expect(account.balance).to eq(50) 
       end
     end
   end
